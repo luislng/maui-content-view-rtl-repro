8fff6e1b4dc4b09b454b24044c1af60ca4484dea
luislng <143736654+luislng@users.noreply.github.com>
2023-05-19 12:23:17 -0600

    Added workaround

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
 
 namespace MauiArabic;
 
@@ -13,12 +14,36 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .ConfigureMauiHandlers(handlers =>
+            {
+#if IOS
+                // Workaround from Shane and EZ
+                ContentViewHandler.Mapper.AppendToMapping(nameof(IContentView.Content), (handler, view) =>
+                {
+                      if (view is ContentView cv && cv.Content != null)
+                      {
+                            cv.FlowDirection = FlowDirection.LeftToRight;
+
+                            switch (Microsoft.Maui.ApplicationModel.AppInfo.RequestedLayoutDirection)
+                            {
+                                  case LayoutDirection.LeftToRight:
+                                        cv.Content.FlowDirection = FlowDirection.LeftToRight;
+                                        break;
+                                  case LayoutDirection.RightToLeft:
+                                        cv.Content.FlowDirection = FlowDirection.RightToLeft;
+                                        break;
+                            }
+                      }
+                });
+#endif
+            });
+
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
+        return builder.Build();
 	}
 }
